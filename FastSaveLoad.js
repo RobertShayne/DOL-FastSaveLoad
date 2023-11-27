7f2ec573df10fd59c20d3d73bf5be72510948eb5
Shayne <realshayne@gmail.com>
2023-11-27 20:39:48 +0800

    first edition

---
+++ FastSaveLoad.js
@@ -0,0 +1,22 @@
+document.addEventListener('keydown', function(event) {
+    if(event.key === '`')
+    {
+        //alert("load");
+        var _slot = 0;
+        Save.slots.load(_slot);
+    }
+    else if(event.key === '~')
+    {
+        var _slot = 0;
+        //alert("save");
+        //DoLSave.prepareSaveDetails();
+        var saveId = random(10000, 99999);
+        //var data = Save.slots.get(_slot);
+        const success = Save.slots.save(_slot, null, {
+            saveId: V.saveId,
+            saveName: "fastsave",
+            ironman: V.ironmanmode,
+        });
+        //alert("success");
+    }
+});
